3ec5205d539b823c797a20a65c1bfe2ac8fb78ce
Raul Jimenez <rjbarco@gmail.com>
2022-08-04 08:56:08 +0200

    webpack configured for development (assets management, output management, development)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import './style.css';
+import printMe from './model.js';
+
 console.log('Testing webpack working...');
+printMe();
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        index: './src/index.js',
+        model: './src/model.js',
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        runtimeChunk: 'single',
     },
     module: {
         rules: [
@@ -14,6 +32,10 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource',
             },
         ],
